d207f35e7c5158a10657d327447be2809f3ce2cf
rahul khattri <rahulkhattri14@gmail.com>
2023-03-08 21:02:11 +0000

    still working on gui

--- gui.py
+++ gui.py
@@ -1,10 +1,35 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk,Image
 from tkhtmlview import HTMLLabel
+import threading
+import algo
+# algo object
+ob = algo.cryp()
 #main window object
 root = Tk()
-root.minsize(400,400)
+bgimg= ImageTk.PhotoImage(Image.open("assets/bg.jpg"))
+bgLabel = Label(root,image=bgimg)
+bgLabel.place(x=0,y=0,relheight=1,anchor="nw")
+root.minsize(500,500)
 root.title("Cryptarithmetic Problem Solver")
+#submit
+
+def validate():
+    string = strings.get()
+    res = resultval.get()
+    string=string.upper()
+    res=res.upper()
+    l = string.split(sep=",")
+    if(len(l)>0 and res!=""):
+      #starting this in a background thread coz else this will keep executing in the main thread and block it after few seconds.
+      bgThread = threading.Thread(target=submit,args=(l,res))
+      bgThread.start()
+      
+    else:
+       messagebox.showerror("Error!","Please fill out both the fields")
+def submit(l,res):
+   print(ob.isSolvable(l,res))
 #menu bar
 def abtCmd():
     about = Toplevel()
@@ -26,16 +51,14 @@ file_menu.add_command(label="About",command=abtCmd)
 file_menu.add_separator()
 file_menu.add_command(label="Exit",command=root.destroy)
 # root.iconbitmap(bitmap="C:/Users/hp/Desktop/python stuff/cryparith/Cryptarithmetic-Problem-solver-main/creative.png")
-# myImg = ImageTk.PhotoImage(Image.open("thinking.png"))
-# imglabel = Label(image=myImg).grid(row=1,column=2)
-Label(text="Cryptarithmetic Problem Solver!").grid(row=0,column=2)
-number = Label(root,text="Input Strings\n(Pease enter minimum 2 strings,separated by commas)").grid(row=3,column=1)
-result = Label(root,text="Result String").grid(row=4,column=1)
-numberval = IntVar()
+myImg = ImageTk.PhotoImage(Image.open("thinking.png"))
+Label(text="Cryptarithmetic Problem Solver!".upper(),bg="#c7cee8").pack()
+imglabel = Label(image=myImg,padx=10,pady=20,bg="#c7cee8").place(x=220,y=30)
+number = Label(root,text="Input Strings",bg="#c7cee8").place(x=100,y=150)
+result = Label(root,text="Result String",bg="#c7cee8").place(x=100,y=200)
+strings = StringVar()
 resultval = StringVar()
-print(type(numberval))
-numberentry = Entry(root,textvariable=numberval)
-resultentry = Entry(root,textvariable=resultval)
-numberentry.grid(row=3,column=2)
-resultentry.grid(row=4,column=2)
+numberentry = Entry(root,textvariable=strings).place(x=210,y=150)
+resultentry = Entry(root,textvariable=resultval).place(x=210,y=200)
+mybtn = Button(text="SUBMIT",command=validate).place(x=230,y=250)
 root.mainloop()
